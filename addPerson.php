193d2898777e362f02cd9a6d7d5d423a39b088cf
Jose <murrietj@oregonstate.edu>
2017-08-11 21:39:18 -0700

    first commit

---
+++ addPerson.php
@@ -0,0 +1,21 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 'On');
+//Connects to the database
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","fatehie-db","IuBhtRIUUdodcvXy","fatehie-db");
+if(!$mysqli || $mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	
+if(!($stmt = $mysqli->prepare("INSERT INTO characters(first_name, last_name, house) VALUES (?,?,?)"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!($stmt->bind_param("ssi",$_POST['FirstName'],$_POST['LastName'],$_POST['House']))){
+	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+} else {
+	echo "Added " . $stmt->affected_rows . " rows to characters.";
+}
+?>
