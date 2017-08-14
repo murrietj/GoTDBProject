dbd6918785d69ecf5ea2e1f357e07c1d48fa6f64
Jose <murrietj@oregonstate.edu>
2017-08-13 18:35:54 -0700

    Added the rest of the tables and their add forms

---
+++ addHouse.php
@@ -0,0 +1,21 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 'On');
+//Connects to the database
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","fatehie-db","IuBhtRIUUdodcvXy","fatehie-db");
+if(!$mysqli || $mysqli->connect_errno){
+	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+	}
+	
+if(!($stmt = $mysqli->prepare("INSERT INTO house(name, sigil, region) VALUES (?,?,?)"))){
+	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!($stmt->bind_param("ssi",$_POST['name'],$_POST['sigil'],$_POST['region']))){
+	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+}
+if(!$stmt->execute()){
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+} else {
+	echo "Added " . $stmt->affected_rows . " rows to houses.";
+}
+?>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if($mysqli->connect_errno){
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
   <body>
-    <div>
+    <fieldset>
       <table>
         <tr>
           <th>Characters</th>
@@ -35,23 +35,18 @@ if(!$stmt->bind_result($first_name, $last_name, $house)){
   echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
 }
 while($stmt->fetch()){
-  echo "<tr>\n<td>\n" . $first_name . "\n</td>\n<td>\n" . $last_name . "\n</td>\n<td>\n" . $house . "\n</td>\n</tr>";
+  echo "<tr>\n<td>" . $first_name . "</td>\n<td>" . $last_name . "</td>\n<td>" . $house . "</td>\n</tr>\n";
 }
 $stmt->close();
 ?>
 
       </table>
-    </div>
-
-    <div>
       <fieldset>
-        <legend>Character</legend>
-        <fieldset>
-          <legend>Add</legend>
-          <form method="post" action="addChar.php"> 
-            <p>First Name: <input type="text" name="FirstName" /></p>
-            <p>Last Name: <input type="text" name="LastName" /></p>
-            <p>House: <select name="House">
+        <legend>Add</legend>
+        <form method="post" action="addChar.php"> 
+          <p>First Name: <input type="text" name="FirstName" /></p>
+          <p>Last Name: <input type="text" name="LastName" /></p>
+          <p>House: <select name="House">
 
 <?php
 if(!($stmt = $mysqli->prepare("SELECT id, name FROM house"))){
@@ -61,97 +56,228 @@ if(!($stmt = $mysqli->prepare("SELECT id, name FROM house"))){
 if(!$stmt->execute()){
   echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
 }
-if(!$stmt->bind_result($id, $pname)){
+if(!$stmt->bind_result($id, $name)){
   echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
 }
 while($stmt->fetch()){
-  echo '<option value=" '. $id . ' "> ' . $pname . '</option>\n';
+  echo "<option value='". $id . "'>". $name . "</option>\n";
 }
 $stmt->close();
 ?>
 
-            </select></p>
-            <p><input type="submit" value="Add"/></p>
-          </form>
-        </fieldset>
-        
-        <fieldset>
-          <legend>Update</legend>
-          <form method="post" action="updateChar.php"> 
-            <p>Character: <select name="Character">
-
-<?php
-if(!($stmt = $mysqli->prepare("SELECT id, first_name, last_name FROM characters"))){
-  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-}
-
-if(!$stmt->execute()){
-  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-if(!$stmt->bind_result($id, $fname, $lname)){
-  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-while($stmt->fetch()){
-  echo '<option value=" '. $id . ' "> ' . $fname . ' ' . $lname . '</option>\n';
-}
-$stmt->close();
-?>
-
-            </select></p>
-            <p>First Name: <input type="text" name="FirstName" /></p>
-            <p>Last Name: <input type="text" name="LastName" /></p>
-            <p>House: <select name="House">
-
-<?php
-if(!($stmt = $mysqli->prepare("SELECT id, name FROM house"))){
-  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-}
-
-if(!$stmt->execute()){
-  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-if(!$stmt->bind_result($id, $pname)){
-  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-while($stmt->fetch()){
-  echo '<option value=" '. $id . ' "> ' . $pname . '</option>\n';
-}
-$stmt->close();
-?>
-
-            </select></p>
-            <p><input type="submit" value="Update" /></p>
-          </form>
-        </fieldset>
-
-        <fieldset>
-          <legend>Delete</legend>
-          <form method="post" action="deleteChar.php"> 
-            <p>Character: <select name="Character">
-
-<?php
-if(!($stmt = $mysqli->prepare("SELECT id, first_name, last_name FROM characters"))){
-  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-}
-
-if(!$stmt->execute()){
-  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-if(!$stmt->bind_result($id, $fname, $lname)){
-  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
-}
-while($stmt->fetch()){
-  echo '<option value=" '. $id . ' "> ' . $fname . ' ' . $lname . '</option>\n';
-}
-$stmt->close();
-?>
-
-            </select></p>
-            <p><input type="submit" value="Delete" /></p>
-          </form>
-        </fieldset>
-        
+          </select></p>
+          <p><input type="submit" value="Add"/></p>
+        </form>
       </fieldset>
-    </div>	
+
+      <fieldset>
+        <legend>Update</legend>
+        <form method="post" action="updateChar.php"> 
+          <p>Character: <select name="Character">
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, first_name, last_name FROM characters"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $fname, $lname)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<option value='". $id . "'>" . $fname . " " . $lname . "</option>\n";
+}
+$stmt->close();
+?>
+
+          </select></p>
+          <p>First Name: <input type="text" name="FirstName" /></p>
+          <p>Last Name: <input type="text" name="LastName" /></p>
+          <p>House: <select name="House">
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, name FROM house"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $name)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<option value='". $id . "'>" . $name . "</option>\n";
+}
+$stmt->close();
+?>
+
+          </select></p>
+          <p><input type="submit" value="Update" /></p>
+        </form>
+      </fieldset>
+
+      <fieldset>
+        <legend>Delete</legend>
+        <form method="post" action="deleteChar.php"> 
+          <p>Character: <select name="Character">
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, first_name, last_name FROM characters"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $fname, $lname)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<option value='". $id . "'>" . $fname . " " . $lname . "</option>\n";
+}
+$stmt->close();
+?>
+
+          </select></p>
+          <p><input type="submit" value="Delete" /></p>
+        </form>
+      </fieldset>
+
+    </fieldset>
+
+    <fieldset>
+      <table>
+        <tr>
+          <th>Houses</th>
+        </tr>
+        <tr>
+          <td>Name</td>
+          <td>Sigil</td>
+          <td>Region</td>
+        </tr>
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT house.name, house.sigil, region.name FROM house INNER JOIN region ON house.region = region.id"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($name, $sigil, $region)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<tr>\n<td>" . $name . "</td>\n<td>" . $sigil . "</td>\n<td>" . $region . "</td>\n</tr>\n";
+}
+$stmt->close();
+?>
+
+      </table>
+      <fieldset>
+        <legend>Add</legend>
+        <form method="post" action="addHouse.php"> 
+          <p>Name: <input type="text" name="name" /></p>
+          <p>Sigil: <input type="text" name="sigil" /></p>
+          <p>Region: <select name="region">
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT id, name FROM region"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($id, $name)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<option value='". $id . "'>". $name . "</option>\n";
+}
+$stmt->close();
+?>
+
+          </select></p>
+          <p><input type="submit" value="Add"/></p>
+        </form>
+      </fieldset>
+    </fieldset>
+
+    <fieldset>
+      <table>
+        <tr>
+          <th>Regions</th>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT name FROM region"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($name)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<tr>\n<td>" . $name . "</td>\n</tr>\n";
+}
+$stmt->close();
+?>
+
+      </table>
+      <fieldset>
+        <legend>Add</legend>
+        <form method="post" action="addRegion.php"> 
+          <p>Name: <input type="text" name="name" /></p>
+          <p><input type="submit" value="Add"/></p>
+        </form>
+      </fieldset>
+  </fieldset>
+
+  <fieldset>
+      <table>
+        <tr>
+          <th>Religions</th>
+        </tr>
+        <tr>
+          <td>Name</td>
+        </tr>
+
+<?php
+if(!($stmt = $mysqli->prepare("SELECT name FROM religion"))){
+  echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+}
+
+if(!$stmt->execute()){
+  echo "Execute failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+if(!$stmt->bind_result($name)){
+  echo "Bind failed: "  . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+while($stmt->fetch()){
+  echo "<tr>\n<td>" . $name . "</td>\n</tr>\n";
+}
+$stmt->close();
+?>
+
+      </table>
+      <fieldset>
+        <legend>Add</legend>
+        <form method="post" action="addReligion.php"> 
+          <p>Name: <input type="text" name="name" /></p>
+          <p><input type="submit" value="Add"/></p>
+        </form>
+      </fieldset>
+  </fieldset>
+
   </body>
 </html>
