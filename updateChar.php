f805fc754a9f81c728284b0c3abd49a94c3278e3
Jose <murrietj@oregonstate.edu>
2017-08-16 16:57:29 -0700

    fixed updateChar to handle NULL first name

--- updateChar.php
+++ updateChar.php
@@ -2,28 +2,31 @@
 //Turn on error reporting
 ini_set('display_errors', 'On');
 //Connects to the database
-$mysqli = new mysqli("oniddb.cws.oregonstate.edu","fatehie-db","IuBhtRIUUdodcvXy","fatehie-db");
-if(!$mysqli || $mysqli->connect_errno){
-	echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
-	}
-	
-if(!($stmt = $mysqli->prepare("UPDATE characters SET first_name=?, last_name=?, gender=?, house=? WHERE id=?"))){
-	echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
-}
+if(!$_POST['FName'] == ""){
+  $mysqli = new mysqli("oniddb.cws.oregonstate.edu","fatehie-db","IuBhtRIUUdodcvXy","fatehie-db");
+  if(!$mysqli || $mysqli->connect_errno){
+    echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+  }
 
-if($_POST['FName'] == "")
-  $_POST['FName'] = NULL;
-if($_POST['LName'] == "")
-  $_POST['LName'] = NULL;
-if($_POST['House'] == -1)
-  $_POST['House'] = NULL;
+  if(!($stmt = $mysqli->prepare("UPDATE characters SET first_name=?, last_name=?, gender=?, house=? WHERE id=?"))){
+    echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+  }
 
-if(!($stmt->bind_param("sssii",$_POST['FName'],$_POST['LName'],$_POST['Gender'],$_POST['House'],$_POST['Character']))){
-	echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+  if($_POST['LName'] == "")
+    $_POST['LName'] = NULL;
+  if($_POST['House'] == -1)
+    $_POST['House'] = NULL;
+
+  if(!($stmt->bind_param("sssii",$_POST['FName'],$_POST['LName'],$_POST['Gender'],$_POST['House'],$_POST['Character']))){
+    echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+  }
+  if(!$stmt->execute()){
+    echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+  } else {
+    echo "Updated " . $stmt->affected_rows . " rows from characters.";
+  }
 }
-if(!$stmt->execute()){
-	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
-} else {
-	echo "Updated " . $stmt->affected_rows . " rows from characters.";
+else {
+  echo "First name cannot be empty";
 }
 ?>
